6df31e4f9d82958fd1224edc0853962d68949ced
Christian Poma <christianrogerpm@gmail.com>
2019-08-22 15:45:46 -0500

    stop scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('enviar:Emails')
-                 ->everyFiveMinutes();
+        // $schedule->command('enviar:Emails')
+        //          ->everyFiveMinutes();
     }
 
     /**
